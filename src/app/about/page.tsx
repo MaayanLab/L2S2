2fe9a71f309626a7e81fd3b0e72382bc4c31a95f
giacomomarino <77803629+giacomomarino@users.noreply.github.com>
2024-11-18 13:31:53 -0500

    update about page
    fixes #21 and fixes #20 and fixes #18

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,18 +15,13 @@ export async function generateMetadata(props: {}, parent: ResolvingMetadata): Pr
 export default async function About() {
   return (
     <div className="prose">
-      <h2 className="title text-xl font-medium mb-3">About Rummagene</h2>
+      <h2 className="title text-xl font-medium mb-3">About L2S2</h2>
       <div className="flex">
         <div className="flex-col">
         <Image className={'rounded float-right ml-5'} src={'/images/LINCSearch_logo.png'} width={250} height={250} alt={'L2S2'}></Image>
-          <p className="text-justify">
-          
-          </p>
-          
           <br></br>
           <p>
-          The Library of Integrated Network-Based Cellular Signatures (LINCS) is an NIH Common Fund initiative dedicated to cataloging the global responses of human cells to various chemical, genetic, and disease-related perturbations. LINCS resources encompass experimental and computational techniques, data visualization tools, molecular and imaging data, and distinctive cellular response signatures. By compiling a comprehensive view of human cellular responses to diverse perturbations, LINCS aims to enhance our understanding of disease mechanisms and facilitate new therapeutic discoveries. Perturbations studied include drugs, genetic modifications, tissue micro-environments, antibodies, and mutations associated with diseases. Cellular responses to these perturbations are measured using transcript profiling, mass spectrometry, cell imaging, and other biochemical assays. The program emphasizes cellular functions common to different tissues and cell types that are relevant to diseases like cancer, heart disease, and neurodegenerative disorders.
-          Gene set signatures are sourced from SigCom LINCS, a web-based search engine that serves over 1.5 million gene expression signatures processed, analyzed, and visualized from LINCS, GTEx, and GEO. SigCom LINCS provides rapid signature similarity search for mimickers and reversers given sets of up and down genes. L2W2 provides fast enrichment search and term search for over 1.4 million chemical perturbation and over 280,000 CRISPR knockout signatures.
+          The Library of Integrated Network-Based Cellular Signatures (LINCS) Common Fund program was established to construct a reference database of human cancer cell lines responses to diverse multi-omics and phenotypic perturbations. As part of the LINCS initiative, 248 cell lines were profiled with the L1000 transcriptomics assay to measure the response to 33,621 small molecules and 7,508 single gene CRISPR knockouts (KOs). From this massive dataset, we computed sets of up- and down-regulated genes. These gene sets are served for search by the LINCS L1000 Signature Search (L2S2) platform. L2S2 provides instant results when searching across over 1.678 million chemical perturbations and CRISPR KO signatures. The platform includes filters for FDA-approved drugs and signature directionality. With the L2S2 search engine, users can identify small molecules and single gene CRISPR KOs that produce gene expression profiles similar or opposite to their submitted gene sets. Significant CRISPR KO signatures can reveal potential driver genes and drug targets. Similarly, approved drugs and preclinical small molecule signatures that might reverse or mimic a desired expression pattern can provide hypotheses for preclinical experimental validation studies. L2S2 also includes a consensus search feature which ranks specific perturbations across cellular contexts, time points, and concentrations. L2S2 was systematically applied to the CPTAC3 cohort to prioritize potential targets, approved drugs, and preclinical compounds for all previously identified multi-omic cancer cohort subtypes. Overall, L2S2 provides cancer researchers with a powerful tool to further understand and personalize treatments for cancer subtypes.
           </p>
           <br />
           <p>
@@ -34,16 +29,32 @@ export default async function About() {
           </p>
           <br />
           <p>
-          L2S2 was developed by <a className='underline cursor' href="https://labs.icahn.mssm.edu/maayanlab/">the Ma&apos;ayan Lab</a>
+          L2S2 was developed by <a className='underline cursor' href="https://labs.icahn.mssm.edu/maayanlab/">the Ma&apos;ayan Lab.</a>
           </p>
           <br />
-          <p>Please acknowledge LINCS in your publications by citing the following references:</p>
-          <br></br>
-          <p>Evangelista, J. E., Clarke, D. J. B., Xie, Z., Lachmann, A., Jeon, M., Chen, K., Jagodnik, K. M., Jenkins, S. L., Kuleshov, M. V., Wojciechowicz, M. L., Schürer, S. C., Medvedovic, M., & Ma&apos;ayan, A. (2022). SigCom LINCS: data and metadata search engine for a million gene expression signatures. Nucleic acids research, 50(W1), W697–W709.
+          <p>To learn more about the LINCS program and the L1000 assay:</p>
+          <ul className='list-decimal'>
+            <li>The L1000 data was generated by the Connectivity Map at the Broad Institute: <a className='underline cursor' href="https://clue.io/" target='_blank'>https://clue.io/</a>.</li>
+            <li>The LINCS program information portal: <a className='underline cursor' href="https://lincsproject.org/" target='_blank'>https://lincsproject.org/</a>.</li>
+            <li>The LINCS program landmark paper:<p className='text-sm'>Keenan, A. B., et al. (2018). The Library of Integrated Network-Based Cellular Signatures NIH Program: System-Level Cataloging of Human Cells Response to Perturbations. Cell systems, 6(1), 13–24. 
+              <a className='underline cursor' href="https://doi.org/10.1016/j.cels.2017.11.001" target='_blank'>https://doi.org/10.1016/j.cels.2017.11.001</a></p></li>
+            <li>Alternative LINCS L1000 data portals and search engines:</li>
+            a. SigCom LINCS <a href='https://maayanlab.cloud/sigcom-lincs' target="_blank" className='underline cursor'>https://maayanlab.cloud/sigcom-lincs</a> publication:<br></br>
+            <p className='text-sm'>Evangelista, J. E., et al. (2022). SigCom LINCS: data and metadata search engine for a million gene expression signatures. Nucleic acids research, 50(W1), W697–W709.
             <a className="underline cursor" href="https://doi.org/10.1093/nar/gkac3287" target="_blank"> https://doi.org/10.1093/nar/gkac3287.</a></p>
-            <br></br>
-          <p>Keenan, A. B., Jenkins, S. L., Jagodnik, K. M., Koplev, S., He, E., Torre, D., Wang, Z., Dohlman, A. B., Silverstein, M. C., Lachmann, A., Kuleshov, M. V., Ma&apos;ayan, A., Stathias, V., Terryn, R., Cooper, D., Forlin, M., Koleti, A., Vidovic, D., Chung, C., Schürer, S. C., … Pillai, A. (2018). The Library of Integrated Network-Based Cellular Signatures NIH Program: System-Level Cataloging of Human Cells Response to Perturbations. Cell systems, 6(1), 13–24.
-            <a className="underline cursor" href="https://doi.org/10.1016/j.cels.2017.11.001" target="_blank"> https://doi.org/10.1016/j.cels.2017.11.001.</a></p>
+            b. iLINCS <a href='https://www.ilincs.org/' target="_blank" className='underline cursor'>https://www.ilincs.org/</a> publication:<br></br>
+            <p className='text-sm'>Pilarczyk, M., et al. (2022). Connecting omics signatures and revealing biological mechanisms with iLINCS. Nature communications, 13(1), 4678. <a href='https://doi.org/10.1038/s41467-022-32205-3' target="_blank" className="underline cursor">https://doi.org/10.1038/s41467-022-32205-3</a></p>
+            c. LINCS Data Portal <a href='https://lincsportal.ccs.miami.edu/dcic-portal/' target="_blank" className='underline cursor'>https://lincsportal.ccs.miami.edu/dcic-portal/</a> publication:<br></br>
+            <p className='text-sm'>
+            Stathias, V., et al. (2020). LINCS Data Portal 2.0: next generation access point for perturbation-response signatures. Nucleic acids research, 48(D1), D431–D439.
+            <a href='https://doi.org/10.1093/nar/gkz1023' target="_blank" className='underline cursor'>https://doi.org/10.1093/nar/gkz1023</a></p>
+            d. L1000FWD <a href='https://maayanlab.cloud/l1000fwd/' target="_blank" className='underline cursor'>https://maayanlab.cloud/l1000fwd/</a> publication:<br></br>
+            Wang, Z., Lachmann, A., Keenan, A. B., & Ma&apos;ayan, A. (2018). L1000FWD: fireworks visualization of drug-induced transcriptomic signatures. Bioinformatics (Oxford, England), 34(12), 2150–2152.
+            <a href=' https://doi.org/10.1093/bioinformatics/bty060' target="_blank" className='underline cursor'> https://doi.org/10.1093/bioinformatics/bty060</a><br></br>
+            e. L1000CDS2 <a href='https://maayanlab.cloud/L1000CDS2' target="_blank" className='underline cursor'>https://maayanlab.cloud/L1000CDS2</a> publication:<br></br>
+            <p className='text-sm'>Duan, Q., et al. (2016). L1000CDS2: LINCS L1000 characteristic direction signatures search engine. NPJ systems biology and applications, 2, 16015–. <a href='https://doi.org/10.1038/npjsba.2016.15' target="_blank" className='underline cursor'>https://doi.org/10.1038/npjsba.2016.15</a></p>
+          </ul>
+          <br></br>
         </div>
        
       </div>
