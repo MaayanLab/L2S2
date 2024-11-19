c04de966a821d551a7e871490f4b8da33b70e579
giacomomarino <77803629+giacomomarino@users.noreply.github.com>
2024-11-19 14:43:08 -0500

    update fda approved
    fixes #11

---
+++ bot/helper/cli/update_fda_approved.py
@@ -0,0 +1,43 @@
+from tqdm import tqdm
+from helper.cli import cli
+
+
+def import_count_fda_info(plpy):
+    import json
+    with open("data/counts_perts.json") as f:
+        counts_perts = json.load(f)
+    with open("data/fda_approved_new.json") as f:
+        fda_drugs = json.load(f)
+
+    # Construct SQL query
+    sql_query = """
+        INSERT INTO app_public_v2.fda_counts (perturbation, count, approved)
+        VALUES (%s, %s, %s)
+        ON CONFLICT (perturbation)
+        DO UPDATE SET
+            count = EXCLUDED.count,
+            approved = EXCLUDED.approved;
+    """
+
+    # Prepare data as a list of tuples
+    data = [
+        (pert, int(counts_perts[pert]), pert.upper() in fda_drugs)
+        for pert in counts_perts
+    ]
+
+    # Execute in chunks if necessary
+    with plpy.conn.cursor() as cursor:
+        for row in tqdm(data, desc="Inserting chemical perturbation info.."):
+            cursor.execute(sql_query, row)
+        
+        
+@cli.command()
+def ingest_counts_fda():
+    from helper.plpy import plpy
+    try:
+        import_count_fda_info(plpy)
+    except:
+        plpy.conn.rollback()
+        raise
+    else:
+        plpy.conn.commit()
