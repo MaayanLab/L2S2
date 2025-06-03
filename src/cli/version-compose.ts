9001e706cc73096f400b549b71b43544afe384ab
Daniel J. B. Clarke <u8sand@gmail.com>
2025-06-03 13:33:03 -0400

    Fixup version control

--- src/cli/version-compose.ts
+++ src/cli/version-compose.ts
@@ -2,5 +2,5 @@ import fs from 'fs'
 import path from 'path'
 
 let compose = fs.readFileSync(path.join(__dirname, '..', '..', 'docker-compose.yaml'), { encoding: 'utf-8' })
-compose = compose.replaceAll(/maayanlab\/rummagene:[\d\.]+/g, `maayanlab/rummagene:${process.env.npm_package_version}`)
+compose = compose.replaceAll(/maayanlab\/l2s2-app:[\d\.]+/g, `maayanlab/l2s2-app:${process.env.npm_package_version}`)
 fs.writeFileSync(path.join(__dirname, '..', '..', 'docker-compose.yaml'), compose)
